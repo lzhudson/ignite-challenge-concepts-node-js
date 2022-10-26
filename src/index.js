e0b579db3053133a048fcd8e47b4b8bbdf9ded97
Hudson Holanda <hudsonholanda55@gmail.com>
2022-10-26 09:31:58 -0300

    feat: implement middleware to check user is exists and return todos of user

--- src/index.js
+++ src/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  console.log(username);
+  
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(400).json({ error: "User not found!" });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -30,7 +41,9 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
